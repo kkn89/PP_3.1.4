a1c92751cc8c12306bec2ea5a1017b36f09b16f8
kkn89 <konstantinkoblyakov@gmail.com>
2022-02-07 23:25:58 +0300

    admin-page

--- src/main/java/com/kata/spring/bootstrap/spring_bootstrap/config/SpringSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.kata.spring.bootstrap.spring_bootstrap.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-    //пустой класс, использующийся для резистрации модуля в спринг-контейнере
-}
